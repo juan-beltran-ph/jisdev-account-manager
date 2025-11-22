e11b0f54d4884b7c095a43419fd710e3241b95d8
Juan Jesus Beltran Ramirez <jisus007@gmail.com>
2025-11-21 20:29:16 -0600

    Adds Lombok config

--- src/main/java/com/jisdev/accountmanager/dto/UserAccountResponse.java
+++ src/main/java/com/jisdev/accountmanager/dto/UserAccountResponse.java
@@ -2,11 +2,15 @@ package com.jisdev.accountmanager.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@Getter
+@Setter
 public class UserAccountResponse {
     private Long id;
     private String username;
