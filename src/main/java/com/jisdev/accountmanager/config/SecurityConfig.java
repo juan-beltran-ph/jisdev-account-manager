0fdf1c83e8fcb3ce2cfa75bba24919bc5534c250
Juan Jesus Beltran Ramirez <jisus007@gmail.com>
2025-11-26 19:35:06 -0600

    Solve h2console access

--- src/main/java/com/jisdev/accountmanager/config/SecurityConfig.java
+++ src/main/java/com/jisdev/accountmanager/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jisdev.accountmanager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(authorize -> authorize
                         //.requestMatchers("/api/**").permitAll()
                         .requestMatchers("/**").permitAll()
@@ -27,4 +29,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+    
 }
